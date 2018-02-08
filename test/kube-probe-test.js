8d4226eeee5f0fcdccd1c534a06318cfaff48b75
Lance Ball <lball@redhat.com>
2018-02-08 09:52:41 -0500

    test: test custom content type for endpoints

--- test/kube-probe-test.js
+++ test/kube-probe-test.js
@@ -99,3 +99,22 @@ test('default content type for liveness endpoint', t => {
       t.end();
     });
 });
+
+test('custom content type for liveness endpoint', t => {
+  t.plan(1);
+  const app = connect();
+  probe(app, {
+    livenessCallback: (request, response) => {
+      response.setHeader('Content-Type', 'application/json');
+      response.end(JSON.stringify({status: 'OK'}));
+    }
+  });
+  supertest(app)
+    .get('/api/health/liveness')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .then(response => {
+      t.strictEqual(response.body.status, 'OK', 'expected response');
+      t.end();
+    });
+});
